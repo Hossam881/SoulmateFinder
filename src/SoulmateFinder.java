ff698ea84e7bcffed6020988566c8968fa212be5
hossambabari <hossam.babari21@gmail.com>
2022-11-03 17:20:45 +0000

    update

--- src/SoulmateFinder.java
+++ src/SoulmateFinder.java
@@ -1,8 +1,52 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 public class SoulmateFinder {
-    public static int Main(){
-        StationStart = TravellerNumber("Where did your journey start?");
-        StationEnd = TravellerNumber("Where did your journey end?");
+     public static void main(String[] args) {
+         int StationStart = TravellerNumber("Where did your journey start?");
+         int StationEnd = TravellerNumber("Where did your journey end?");
+         int size = args.length;
+         int soulmatesNumber = 0;
+         if (size != 0) {
+             for (int i = 0; i < size; i++) {
+                 ArrayList<String> start1 = ReadFile(args[i]);
+                 //System.out.println(start1.size());
+                 for(int x = 0; x<start1.size(); x+=3){
+                     //System.out.println(start1.get(x));
+                     boolean travellers = overlappingJourneys(StationStart, StationEnd, Integer.valueOf(start1.get(x+1)), Integer.valueOf(start1.get(x+2)));
+                     if (travellers ==true){
+                         System.out.println(start1.get(x)+ " might be the soulmate.");
+                         soulmatesNumber +=1;
+
+                     }
+                 }
+             }
+             System.out.println("Number of potential soulmates: " + soulmatesNumber);
+
+         }else {
+             System.out.println("Number of potential soulmates: " + (getTravellers(StationStart, StationEnd)));
+         }
+     }
+    public static ArrayList<String> ReadFile(String filename) {
+        int ItemNumber = 0;
+        ArrayList<String> Array = null;
+        try {
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+            Array = new ArrayList<String>();
+            while (myReader.hasNext()) {
+                String ArrayItem = myReader.next();
+                Array.add(ArrayItem);
+                //ItemNumber = Integer.valueOf(ArrayItem);
+
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("WARNING: " + filename + " not found.");
+        }
+        return Array;
 
     }
     public static String TravellerData(String Question) {
@@ -19,6 +63,17 @@ public class SoulmateFinder {
         int Traveller = Data.nextInt();
         return Traveller;
 
+    }
+
+    public static boolean overlappingLongJourneys(int start1, int end1, int start2, int end2, int n){
+         boolean found = false;
+         int overlap = end1 - start2;
+         if (overlap>=n ){
+             found = true;
+         }
+
+
+return found;
     }
 
     public static boolean overlappingJourneys(int start1, int end1, int start2, int end2){
